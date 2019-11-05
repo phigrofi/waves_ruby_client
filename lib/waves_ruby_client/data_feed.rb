0eed6921309b318b8da51ba822fc75b674046052
Philipp Großelfinger <philipp.grosselfinger@gmail.com>
2019-11-05 11:18:33 +0100

    Migrated to Waves Data Service
    The previously used Waves Market data is not available anymore

--- lib/waves_ruby_client/data_feed.rb
+++ lib/waves_ruby_client/data_feed.rb
@@ -7,10 +7,12 @@ module WavesRubyClient
 
     # get history from data feed
     def self.trade_history(count = 10)
-      WavesRubyClient::Api.instance.call_data_feed("/trades/WAVES/BTC/#{count}").map do |order|
+      WavesRubyClient::Api.instance.call_data_feed("/transactions/exchange?amountAsset=WAVES&limit=#{count}")['data'].map do |entry|
+        order = entry['data']
         order['price'] = order['price'].to_f
-        order['timestamp'] = order['timestamp'] / 1000
+        order['timestamp'] = Time.parse(order['timestamp'])
         order['amount'] = order['amount'].to_f
+        %w[height version proofs fee sender].each{ |v| order.delete(v) }
         WavesRubyClient::Order.new(order)
       end
     end
